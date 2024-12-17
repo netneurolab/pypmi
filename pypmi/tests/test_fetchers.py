8dc2a3f80b63dc9f4ae6f29de34c4052d3bcf022
Zhen-Qi Liu <liuzhenqi77@gmail.com>
2024-12-17 12:20:35 -0500

    Refactor tests in test_fetchers.py to improve structure and update assertions for dataset counts

--- pypmi/tests/test_fetchers.py
+++ pypmi/tests/test_fetchers.py
@@ -1,66 +1,67 @@
 # -*- coding: utf-8 -*-
 """Code for testing the `pypmi` package."""
 
+import os
 import pytest
+import requests
+from pathlib import Path
 
 from pypmi import fetchers
 
-
-@pytest.mark.parametrize('url', [
-    "https://utilities.loni.usc.edu/download/study",
-    "https://utilities.loni.usc.edu/download/genetic"
-])
-def test_get_download_params(url):
+def test_get_download_params():
     """Test that we can retrieve the authorization key."""
+
     # confirm we can retrieve authorization key effectively
-    params = fetchers._get_download_params(url)
-    assert isinstance(params, dict)
-    try:
-        assert all(f in params.keys() for f in ['authKey', 'userId'])
-    except AssertionError:
-        pass
+    session = fetchers._get_auth_session()
+    assert isinstance(session, requests.sessions.Session)
 
     # confirm bad user/password returns NO authorization
-    assert fetchers._get_download_params(url, 'baduser', 'badpass') is None
-
-    # invalid URL raises error
     with pytest.raises(ValueError):
-        fetchers._get_download_params('invalidurl')
+        fetchers._get_auth_session('baduser', 'badpass')
 
 
 def test_fetchable_studydata():
     """Test that we can retrieve the list of available datasets."""
     # 113 datasets available, should get a list of them
     dsets = fetchers.fetchable_studydata()
-    assert isinstance(dsets, list) and len(dsets) == 113
+    assert isinstance(dsets, list) and len(dsets) == 538
 
 
 @pytest.mark.parametrize(('datasets', 'expected'), [
-    (['all'], 113),
-    (['Code list', 'Clinical labs'], 2)
+    (['Code List'], 1),
+    (['Current Biospecimen Analysis Results', 'Data Dictionary'], 2)
 ])
-def test_fetch_studydata(studydata, datasets, expected):
+def test_fetch_studydata(datadir, datasets, expected):
     """Test that we can download datasets."""
-    # ensure dataset download returns expected number of files
-    out = fetchers.fetch_studydata(*datasets, path=studydata, verbose=False)
+    out = fetchers.fetch_studydata(
+        datasets, 
+        path=datadir, 
+        overwrite=True, 
+        verbose=False
+    )
     assert len(out) == expected
 
 
 def test_fetchable_genetics():
     """Test that we can retrieve the list of available datasets."""
-    # 199 datasets available, should get a list of them
     dsets = fetchers.fetchable_genetics()
-    assert isinstance(dsets, list) and len(dsets) == 199
-
-    # requesting projects will return a shorter list
-    projects = fetchers.fetchable_genetics(projects=True)
-    assert isinstance(projects, list) and len(projects) == 7
+    assert isinstance(dsets, list) and len(dsets) == 237
 
 
 @pytest.mark.parametrize(('datasets', 'expected'), [
-    (['project 108'], 2)
+    (['Project 272: PPMI Isogenic iPSCs Whole Genome Sequencing'], 1),
+    ([
+        'Project 272: PPMI Isogenic iPSCs Whole Genome Sequencing',
+        'Project 193: Whole Genome Sequencing of iPSC lines'
+    ], 2)
 ])
-def test_fetch_genetics(studydata, datasets, expected):
+def test_fetch_genetics(datadir, datasets, expected):
     """Test that we can download datasets."""
-    out = fetchers.fetch_genetics(*datasets, path=studydata, verbose=False)
+    out = fetchers.fetch_genetics(
+        datasets,
+        path=datadir,
+        overwrite=True,
+        verbose=False
+    )
     assert len(out) == expected
+    assert all([f.is_file() for f in out])
