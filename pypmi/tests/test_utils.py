1adae6f4d4b08ff4078a6b550940ba6fa806975e
Zhen-Qi Liu <liuzhenqi77@gmail.com>
2024-12-18 13:58:41 -0500

    Refactor tests in test_utils.py to improve function names and enhance data directory checks

--- pypmi/tests/test_utils.py
+++ pypmi/tests/test_utils.py
@@ -2,47 +2,68 @@
 """Code for testing the `pypmi` package."""
 
 import os
+from pathlib import Path
 import pytest
 
 from pypmi import utils
+from pypmi.fetchers import fetch_studydata, _STUDYDATA
 
+TEST_PPMI_PATH = Path(os.environ['TEST_PPMI_PATH'])
 
-def test_get_authentication():
+def test_get_cred():
     """Test that we can retrieve the authentication."""
     # confirm fetching from environment works
     try:
-        assert utils._get_authentication() is not None
+        assert utils._get_cred() is not None
     except AssertionError:
         pass
     # confirm providing only one input still fetches both (from environ)
     try:
-        assert utils._get_authentication(user='user') != ('user', None)
-        assert utils._get_authentication(password='pass') != (None, 'pass')
+        assert utils._get_cred(user='user') != ('user', None)
+        assert utils._get_cred(password='pass') != (None, 'pass')
     except AssertionError:
         pass
     # confirm giving both inputs simply returns inputs, as provided
-    assert utils._get_authentication('user', 'pass') == ('user', 'pass')
+    assert utils._get_cred('user', 'pass') == ('user', 'pass')
 
 
-def test_get_data_dir(studydata):
+def test_get_data_dir():
     """Test that we can retrieve the data directory."""
     ppmi_path = os.environ.get('PPMI_PATH')
     if ppmi_path is not None:
         del os.environ['PPMI_PATH']
 
     # no path and $PPMI_PATH not set returns current directory
-    assert os.getcwd() == utils._get_data_dir()
+    assert Path.cwd() == utils._get_data_dir()
     # setting $PPMI_PATH uses that instead of the current directory
-    os.environ['PPMI_PATH'] = str(studydata)
-    assert str(studydata) == utils._get_data_dir()
+    os.environ['PPMI_PATH'] = str(TEST_PPMI_PATH.resolve())
+    assert TEST_PPMI_PATH.resolve() == utils._get_data_dir()
     # providing path returns path
-    assert studydata == utils._get_data_dir(studydata)
-    # providing filename that exists at path provides path
-    assert studydata == utils._get_data_dir(studydata,
-                                            ['AV-133_Image_Metadata.csv'])
-    # providing filename that does not exist at path raises error
-    with pytest.raises(FileNotFoundError):
-        utils._get_data_dir(studydata, fnames=['notafile.csv'])
+    assert TEST_PPMI_PATH.resolve() == utils._get_data_dir(TEST_PPMI_PATH)
 
     if ppmi_path is not None:
         os.environ['PPMI_PATH'] = ppmi_path
+
+def test_check_data_exist(datadir):
+    """Test that we can check if data exists."""
+    # confirm that we can check if data exists
+
+    fname = "Code List"
+    fetch_studydata(
+        [fname], 
+        path=datadir, 
+        overwrite=False, 
+        verbose=True
+    )
+
+    assert utils._check_data_exist(
+        datadir,
+        _STUDYDATA[fname]['filename'],
+        datetoken=None
+    ) == True
+
+    assert utils._check_data_exist(
+        datadir,
+        _STUDYDATA[fname]['filename'],
+        datetoken="01Jan2000"
+    ) == False
